253a405917388b3542f2ec18e017f177e27aebc8
Melli11 <mingerez@gmail.com>
2023-04-25 21:19:31 -0300

    primer commit

---
+++ src/main/java/scripts/sauceDemo/App.java
@@ -0,0 +1,13 @@
+package scripts.sauceDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
